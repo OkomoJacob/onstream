eff65d57fa498a9af7ff73e0ac4265747c7da4ff
okomojacob <okomo.code@gmail.com>
2025-03-21 16:18:41 +0300

    useState Hook: Search movies.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Search from "./components/Search";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  
   return (
     <main>
       <div className="pattern" />
       <div className="wrapper">
         <header>
-        <img src="./hero-img.png" alt="" />
-          <h1>Find <span className="text-gradient">Movies</span> you'll enjoy without the Hassle.</h1>
+          <img src="./hero-img.png" alt="" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> you'll enjoy
+            without the Hassle.
+          </h1>
         </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className="text-white">{searchTerm}</h1>
       </div>
     </main>
   );
