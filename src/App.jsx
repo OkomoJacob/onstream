cc285227b4c3040ffcf3e52273f76d44401d5e14
okomojacob <okomo.code@gmail.com>
2025-03-21 13:23:17 +0300

    Hooks: Dependency array (Deps).

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 const Card = ({ title }) => {
+  const [count, setCount] = useState(0);
   const [isLiked, setIsLiked] = useState(false);
   useEffect(() => {
-    console.log(`${title} has been like: ${isLiked}`);
-  });
+    console.log(`${title} has been liked: ${isLiked}`);
+  }, [isLiked, title]);
+
   return (
-    <div className="card">
-      <h2>{title}</h2>
+    <div className="card" onClick={() => setCount(count + 1)}>
+      <h2>
+        {title} {count || null}
+      </h2>
       <button onClick={() => setIsLiked(!isLiked)}>
         {isLiked ? "❤️" : "🤍"}
       </button>
