071ed0fcd0ba9fe78f0c4487b64b9e3cc44d94e3
okomojacob <okomo.code@gmail.com>
2025-03-24 15:08:03 +0300

    :rocket: Implemented Search logic.

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,14 @@ const App = () => {
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = "") => {
     setIsLoading(true);
     setErrorMsg("");
 
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
       const data = await response.json();
       setMovieList(data.results || []);
@@ -43,8 +45,8 @@ const App = () => {
     }
   };
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(searchTerm);
+  }, [searchTerm]);
 
   return (
     <main>
@@ -60,7 +62,6 @@ const App = () => {
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
         <section className="all-movies">
-          <h4 className="text-white">{searchTerm}</h4>
           <h2 className="mt-[40px]">All Movies</h2>
 
           {isLoading ? (
